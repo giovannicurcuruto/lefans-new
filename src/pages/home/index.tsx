e651f09cc3d6a4dad6f9f6b15d3656050e1e3321
giovannicurcuruto <ggcurcuruto@gmail.com>
2024-12-23 17:39:43 -0300

    Ajuste

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,74 +2,83 @@
 
 import FooterComponent from "../../components/Footer";
 import HeaderComponent from "../../components/Header";
-import { GridFans, GridFoto, GridGrafico, GridMedium, GridMediumFotoText, GridMediumGrafico, HomePageBody, Teste1 } from "./style";
+import { GridFans, GridFoto, GridGrafico, GridMedium, GridMediumFotoText, GridMediumGrafico, HomePageBody, ImgBgElefante, Teste1, Wrapper } from "./style";
 
 import elefante from '../../assets/elefante_grande.png';
 import foto1 from '../../assets/foto1.png';
 import grafico1 from '../../assets/grafico_financeiro.png';
+import bgelefa from '../../assets/sihueta.png';
 
-// }
+
+
 export default function HomePage() {
     return (
         <>
             <Teste1>
+                <ImgBgElefante src={bgelefa} />
                 <HomePageBody>
-                    <HeaderComponent />
-                    <GridFans>
-                        <div>
-                            <img src={elefante} alt="" />
+                    
+                    <Wrapper>
+                        <HeaderComponent />
 
-                        </div>
-                        <GridMedium>
+                        <GridFans >
                             <div>
-                                <h3>Pessoas, Saúde e Patrimônio</h3>
-                            </div>
-                            <div>Proteção inteligente e inovadora em tudo que importa você!</div>
-                            <div>
-                                <button>Saiba Mais</button>
-                            </div>
-                        </GridMedium>
-                    </GridFans>
-                    <GridFoto>
-                        <GridMediumFotoText>
-                            <div>
-                                <h3>Cuidando das pessoas, protegendo empresas.</h3>
-                            </div>
-                            <div>A Lefan trabalha de forma integral identificando gerenciando e acompanahdno riscos relacionados à saúde corporativa, de pessoas e de patrimónios.</div>
-                            <div>Atuamos de forma estratégica, gerando indicadores para uma melhor tomada de decisão, com foco em mapear as reais necessidades para sua empresa. </div>
-                            <div>
-                                <button>Se você é profissional de RH, ENTRE EM CONTATO!</button>
-                            </div>
-                        </GridMediumFotoText>
-                        <div>
-                            <img src={foto1} alt="" />
-                        </div>
-                    </GridFoto>
-                    <GridGrafico>
-                        <div>
-                            <img src={grafico1} alt="" />
-                        </div>
-                        <GridMediumGrafico>
-                            <div>
-                                <h3>Decisões baseadas em dados</h3>
-                            </div>
-                            <div>
-                                Nosso objetivo é entregar soluções completas em Planos de Saúde e Seguros, com relatórios personalizados e dashboards intuitivos para insights entratégicos e redução de custos.
-                            </div>
-                            <div>
-                                Os dados fornecidos ajudam na tomada de decisão e na prevenção de perdas de investimentos, tornando o plano de saúde da sua empresa mais sustentável
-
+                                <img src={elefante} alt="" />
 
                             </div>
+                            <GridMedium>
+                                <div>
+                                    <h3>Pessoas, Saúde e Patrimônio</h3>
+                                </div>
+                                <div>Proteção inteligente e inovadora em tudo que importa você!</div>
+                                <div>
+                                    <button>Saiba Mais</button>
+                                </div>
+                            </GridMedium>
+                        </GridFans>
+                        <GridFoto>
+                            <GridMediumFotoText>
+                                <div>
+                                    <h3>Cuidando das pessoas, protegendo empresas.</h3>
+                                </div>
+                                <div>A Lefan trabalha de forma integral identificando gerenciando e acompanahdno riscos relacionados à saúde corporativa, de pessoas e de patrimónios.</div>
+                                <div>Atuamos de forma estratégica, gerando indicadores para uma melhor tomada de decisão, com foco em mapear as reais necessidades para sua empresa. </div>
+                                <div>
+                                    <button>Se você é profissional de RH, ENTRE EM CONTATO!</button>
+                                </div>
+                            </GridMediumFotoText>
                             <div>
-                                <button> Mande uma mensagem agora!</button>
+                                <img src={foto1} alt="" />
                             </div>
-                        </GridMediumGrafico>
+                        </GridFoto>
+                        <GridGrafico>
+                            <div>
+                                <img src={grafico1} alt="" />
+                            </div>
+                            <GridMediumGrafico>
+                                <div>
+                                    <h3>Decisões baseadas em dados</h3>
+                                </div>
+                                <div>
+                                    Nosso objetivo é entregar soluções completas em Planos de Saúde e Seguros, com relatórios personalizados e dashboards intuitivos para insights entratégicos e redução de custos.
+                                </div>
+                                <div>
+                                    Os dados fornecidos ajudam na tomada de decisão e na prevenção de perdas de investimentos, tornando o plano de saúde da sua empresa mais sustentável
 
 
-                    </GridGrafico>
+                                </div>
+                                <div>
+                                    <button> Mande uma mensagem agora!</button>
+                                </div>
+                            </GridMediumGrafico>
+
+
+                        </GridGrafico>
+                    </Wrapper>
+
+
                 </HomePageBody>
-                
+
             </Teste1>
             <FooterComponent />
         </>
--- src/pages/home/style.ts
+++ src/pages/home/style.ts
@@ -6,24 +6,37 @@ export const Teste1 = styled.div`
     height: 2000px;
     border-radius: 0 0 250px 250px;
     background-color: var(--main-background-color);
-    z-index: 10;
+    z-index: 0;
     margin-bottom: -250px;
 `;
 
-export const HomePageBody = styled.div`
+export const HomePageBody = styled.div`    
+    position: relative;
+    
+    margin: 0 auto;
+    max-width:1395.758px;
+    width: 1200px;
+    
+    
+`;
+
+export const Wrapper = styled.div`
     display: grid;
+    position: relative;
     place-items: center;
     margin: 0 auto;
     max-width:1395.758px;
     width: 1200px;
     background-color: red;
-    
+    //z-index: 2;
+
 `;
 
 export const GridFans = styled.div`
+  position: relative;
   display: grid;
-  grid-template-columns: 2fr 2fr;  
-
+  grid-template-columns: 2fr 2fr;
+  z-index: 3;  
 `;
 
 export const GridMedium = styled.div`
@@ -35,9 +48,10 @@ export const GridMedium = styled.div`
 `;
 
 export const GridFoto = styled.div`
+    position: relative;
     display: grid;
     grid-template-columns: 2fr 2fr;
-
+    z-index: 3;  
 `;
 
 export const GridMediumFotoText = styled.div`
@@ -57,3 +71,19 @@ export const GridMediumGrafico = styled.div`
     grid-template-rows: 2fr 2fr 2fr 1fr;
 
 `;
+
+export const ImgBgElefante = styled.img`
+    
+    position: absolute; /* Para posicionamento relativo ao pai */
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: auto;
+    height: 100%;
+    z-index: 1; /* Certifica-se de que está atrás */
+    max-width: 1400; /* Remove o limite de largura */
+    
+    
+
+`;
+
